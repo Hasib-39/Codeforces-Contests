9defd91fbd24171352ea2fa38e31625435597d0a
Hasib Altaf <hasibaltaf@iut-dhaka.edu>
2024-02-03 21:59:54 +0600

    Cf 922 div2

---
+++ Contest#922_Div2/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, m;
+        cin >> n >> m;
+        cout << n * (m / 2) << endl;
+    }
+
+    return 0;
+}
---
+++ Contest#922_Div2/C.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll unsigned long long
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll a, b, r;
+    }
+}
